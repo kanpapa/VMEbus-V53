669c782664285df53071421f1bd0228c33e0bd75
Kazuhiro Ouchi <goomog@gmail.com>
2026-05-02 15:39:46 +0900

    4チャネルシリアル送信テストを追加

--- SIO/hello_world/makehex.sh
+++ SIO/hello_world/makehex.sh
@@ -2,4 +2,8 @@
 
 nasm -f bin test2.asm -o test2.bin -l test2.lst
 objcopy -I binary -O ihex --change-addresses=0x0000 test2.bin test2.hex
-cp test2.hex  ~/Downloads/.
+
+nasm -f bin sendloop2.asm -o sendloop2.bin -l sendloop2.lst
+objcopy -I binary -O ihex --change-addresses=0x0000 sendloop2.bin sendloop2.hex
+
+cp test2.hex sendloop2.hex ~/Downloads/.
